eefa317b7d32d6158ce7bc4133ee2e8e4779169e
Germain Leignel <germain.leignel@gmail.com>
2020-05-19 18:43:38 +0100

    as i said, first commit

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Material(
       child: Center(
         child: Text("Project recs app"),
       ),
